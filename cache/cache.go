dd10df16a2f77cbfcf7b6bcab4168c2bccb2b3f8
purple <aimeevessel@gmail.com>
2026-07-14 12:43:23 +1000

    Add files via upload
    cache

---
+++ cache/cache.go
@@ -0,0 +1,215 @@
+﻿package cache
+
+import (
+	"sync"
+	"time"
+)
+
+const (
+	//DefaultTTL             = 5 * time.Minute
+	DefaultCleanupInterval = 30 * time.Second
+	DefaultMaxSize         = 1000
+)
+
+type item struct {
+	value      any
+	expiration int64
+	lastAccess int64
+}
+
+type Cache struct {
+	items           map[string]item
+	rw              sync.RWMutex
+	ttl             time.Duration
+	cleanupInterval time.Duration
+	maxSize         int
+	stopChan        chan struct{}
+}
+
+func NewCache(defaultTTL time.Duration, cleanupInterval time.Duration, maxSize int) *Cache {
+	/*if defaultTTL <= 0 {
+		defaultTTL = DefaultTTL
+
+	}*/
+
+	if cleanupInterval <= 0 {
+		cleanupInterval = DefaultCleanupInterval
+	}
+
+	if maxSize <= 0 {
+		maxSize = DefaultMaxSize
+	}
+
+	c := &Cache{
+		items:           make(map[string]item),
+		ttl:             defaultTTL,
+		cleanupInterval: cleanupInterval,
+		stopChan:        make(chan struct{}),
+		maxSize:         maxSize,
+	}
+	go c.backgroundCleanup()
+	return c
+}
+
+func (c *Cache) Stop() {
+	close(c.stopChan)
+}
+
+func (c *Cache) Set(key string, value any, ttl time.Duration) {
+	c.rw.Lock()
+	defer c.rw.Unlock()
+
+	if c.maxSize > 0 && len(c.items) >= c.maxSize {
+		// log.Println("кэш переполнен")
+		c.deleteOldest()
+	}
+
+	if ttl == 0 {
+		ttl = c.ttl
+	}
+
+	var exp int64
+	if ttl > 0 {
+		exp = time.Now().UnixNano() + int64(ttl)
+	} else {
+		exp = 0
+	}
+
+	c.items[key] = item{
+		value:      value,
+		expiration: exp,
+		lastAccess: time.Now().UnixNano(),
+	}
+}
+
+func (c *Cache) Get(key string) (any, bool) {
+	c.rw.Lock()
+	defer c.rw.Unlock()
+	item, isExist := c.items[key]
+
+	if !isExist {
+		return nil, false
+	}
+
+	if item.expiration > 0 && time.Now().UnixNano() > item.expiration {
+		delete(c.items, key)
+		return nil, false
+	}
+
+	item.lastAccess = time.Now().UnixNano()
+	c.items[key] = item
+	return item.value, isExist
+}
+
+func (c *Cache) Delete(key string) {
+	c.rw.Lock()
+	defer c.rw.Unlock()
+
+	delete(c.items, key)
+}
+
+func (c *Cache) Cleanup() {
+	c.rw.Lock()
+	defer c.rw.Unlock()
+
+	now := time.Now().UnixNano()
+
+	for key, item := range c.items {
+		if item.expiration > 0 && now > item.expiration {
+			delete(c.items, key)
+		}
+	}
+}
+
+func (c *Cache) Exists(key string) bool {
+	c.rw.Lock()
+	defer c.rw.Unlock()
+
+	item, exists := c.items[key]
+
+	if !exists {
+		return false
+	}
+
+	if item.expiration > 0 && time.Now().UnixNano() > item.expiration {
+		delete(c.items, key)
+		return false
+	}
+	
+	return true
+}
+
+func (c *Cache) Keys() []string {
+	c.rw.Lock()
+	defer c.rw.Unlock()
+
+	keys := make([]string, 0, len(c.items))
+	
+	for key, item := range c.items {
+		if item.expiration > 0 && time.Now().UnixNano() > item.expiration {
+		delete(c.items, key)
+		continue
+		}
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func (c *Cache) Size() int {
+    	c.rw.RLock()
+   	defer c.rw.RUnlock()
+   	
+	return len(c.items)
+}
+
+func (c *Cache) backgroundCleanup() {
+	ticker := time.NewTicker(c.cleanupInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			c.Cleanup()
+		case <-c.stopChan:
+			return
+		}
+	}
+}
+
+func (c *Cache) deleteOldest() {
+	if len(c.items) == 0 { 
+		return
+	}
+
+	for key, item := range c.items {
+		if item.expiration > 0 && time.Now().UnixNano() > item.expiration {
+			delete(c.items, key)
+			return
+		}
+	}
+
+	var targetKey string
+	var earliestExp int64 = 0
+	found := false
+
+	for key, item := range c.items {
+        	if item.expiration > 0 {
+            
+           		if !found || item.expiration < earliestExp {
+                	earliestExp = item.expiration
+                	targetKey = key
+                	found = true
+            		}
+        	}
+    	}
+
+	if found && targetKey != "" {
+        	delete(c.items, targetKey)
+        	return
+    	}
+
+	for key := range c.items {
+       		delete(c.items, key)
+        	return
+    	}	
+}
---
+++ cache/cache_test.go
@@ -0,0 +1,83 @@
+﻿package cache
+
+import (
+	"time"
+	"testing"
+)
+
+func TestSetAndGet(t *testing.T) {
+	c := NewCache(1*time.Second, 100*time.Millisecond, 10)
+	defer c.Stop()
+
+	c.Set("key1", "value1", 0)
+	val, ok := c.Get("key1")
+	if !ok || val != "value1" {
+		t.Errorf("вместо ожидаемого значения получено %v", val)
+	}
+
+	_, ok = c.Get("not-exist")
+	if ok {
+		t.Errorf("ключ не может существовать")
+	}
+}
+
+func TestTTL(t *testing.T) {
+	c := NewCache(1*time.Second, 100*time.Millisecond, 10)	
+	defer c.Stop()
+
+	c.Set("key", "value", 100*time.Millisecond)
+
+	_, ok := c.Get("key")
+	if !ok {
+		t.Error("ключ еще должен существовать")
+	}
+	
+	time.Sleep(150 * time.Millisecond)
+	
+	_, ok = c.Get("key")
+	if ok {
+		t.Error("ключ не должен существовать после TTL")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	c := NewCache(5*time.Minute, 10*time.Second, 10)
+	defer c.Stop()
+
+	c.Set("key", "value", 0)
+	c.Delete("key")
+
+	_, ok := c.Get("key")
+	if ok {
+		t.Errorf("ключ существует после удаления")
+	}	
+}
+
+func TestMaxSize(t *testing.T) {
+	c := NewCache(5*time.Minute, 10*time.Second, 2)
+	defer c.Stop()
+
+	c.Set("key1", "value1", 0)
+	c.Set("key2", "value2", 0)
+	c.Set("key3", "value3", 0)
+
+	if c.Size() != 2 {
+		t.Errorf("не соблюден ожидаемый размер кэша")
+	}
+}
+
+func TestKeys(t *testing.T) {
+	c := NewCache(5*time.Minute, 10*time.Second, 10)
+	defer c.Stop()
+
+	c.Set("key1", "value1", 0)
+	c.Set("key2", "value2", 0)
+	c.Set("key3", "value3", 0)
+	
+	keys := c.Keys()
+	if len(keys) != 3 {
+		t.Errorf("ожидалось 3 ключа, получено: %d", len(keys))
+	}
+}
+
+
