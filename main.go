185993a8c779a0634dc6cc56a710a915ba047177
purple <aimeevessel@gmail.com>
2026-07-14 12:39:33 +1000

    Add files via upload
    adding main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	
+	"github.com/MedievaLiar/Cache/cache"
+)
+
+func main() {
+    c := cache.NewCache(5*time.Minute, 10*time.Second, 1000)
+    defer c.Stop()
+
+    c.Set("user1", "Ann", 0)
+    c.Set("user2", "Jim", 30*time.Second)
+
+    if val, exists := c.Get("user1"); exists {
+        fmt.Printf("User 1: %v\n", val)
+    }
+
+    keys := c.Keys()
+    fmt.Printf("Все ключи: %v\n", keys)
+}
